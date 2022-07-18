517e78ac99e498172d9bed3bd284ba9cbcc06c7f
Alif Nur Rachman <alifnurrachman907@gmail.com>
2022-07-19 04:09:01 +0700

    Update test-brew-package.rb

--- test-brew-package.rb
+++ test-brew-package.rb
@@ -7,10 +7,14 @@ class TestBrewPackage < Formula
   url "https://github.com/elaif9/test-brew-package/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "689d796657cccb0ecf6ccdb02cf1ba12edb5c4f0742a363bba1c31e2e9a10a93"
   license "MIT"
+  
+  depends_on "yq"
+  depends_on "gh"
+  depends_on "jq"
+  depends_on "bash"
 
   def install
     bin.install "install.sh"
-    system "./configure", *std_configure_args, "--disable-silent-rules"
   end
 
   test do
